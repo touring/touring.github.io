23ecfa5b6ba05bfc1509a9eb80281d22ff3d1058
wangzai <yiwangzai@gmail.com>
2016-06-23 14:18:33 +0800

    update

---
+++ tools/src/js/data/App.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import  "sass/data.scss";
+
+export default class App extends Component {
+	render() {
+		return (
+			<div className="data-block">
+				
+			</div>
+		)
+	}
+}
